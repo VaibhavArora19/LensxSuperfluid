080eeb5a514f1b0e9dd6af62a5d87389aee10388
VaibhavArora19 <aroravaibhav817@gmail.com>
2023-06-25 21:54:20 +0530

    build fixes

--- frontend/components/Layout/Layout.tsx
+++ frontend/components/Layout/Layout.tsx
@@ -1,12 +1,16 @@
 import Navbar from "../Navbar/Navbar";
+import React from "react";
 
-const Layout = ({children}) => {
+type Props = {
+  children: React.ReactNode;
+};
+const Layout = ({ children }: Props) => {
   return (
     <>
       <Navbar />
       {children}
     </>
-  )
+  );
 };
 
 export default Layout;
